cf10f50acc34b820702f96a5fe2c8bad1502ec25
ibrahim_glmz <gulhezibrahim@gmail.com>
2025-05-04 01:36:44 +0300

    Fix base URL for Vercel deployment

--- my_web/vite.config.js
+++ my_web/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my_web/',
+  base: '/',
   build: {
     // Chunk boyutunu optimize et
     chunkSizeWarningLimit: 1000,
